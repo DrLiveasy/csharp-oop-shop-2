24f6c6e695c6a5b37b5d1d28b838a061a4dec237
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-12-01 15:08:08 +0100

    Add project files.

---
+++ CsharpShop2/Prodotto.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpShop2
+{
+
+    public class Prodotto
+    {
+        private string nome;
+        private string descrizione;
+        private int prezzo;
+        private int Codice;
+        private double iva;
+
+        public Prodotto(string nome, string descrizione, int prezzo, double iva)
+        {
+            Random rnd = new Random();
+            this.Codice = rnd.Next(1, 100);
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+
+
+
+        //Getters 
+
+        public string GetNome()
+        {
+            return this.nome;
+        }
+        public string GetDescrizione()
+        {
+            return this.descrizione;
+        }
+
+        public int GetPrezzo()
+        {
+            return this.prezzo;
+        }
+
+        public int GetCodice()
+        {
+            return this.Codice;
+        }
+
+        public double GetIva()
+        {
+            return this.iva;
+        }
+        
+
+
+        //SETTERS
+        public void CambiaNome(string nome)
+        {
+            if (nome != "")
+            {
+                this.nome = nome;
+            }
+            else
+            {
+                Console.WriteLine("il nome non può essere vuoto"); ;
+            }
+        }
+        public void CambiaDiscrezione(string descrizione)
+        {
+            if (descrizione != "")
+            {
+                this.descrizione = descrizione;
+            }
+            else
+            {
+                Console.WriteLine("non hai inserito nessuna descrizione");
+            }
+        }
+        public void CambiaPrezzo(int prezzo)
+        {
+            if (prezzo > 0)
+            {
+                this.prezzo = prezzo;
+            }
+            else
+            {
+                Console.WriteLine("il Prezzo non può essere uguale o minore di 0 ");
+            }
+        }
+        public void CambiaIva(int iva)
+        {
+            if (iva > 0)
+            {
+                this.iva = iva;
+            }
+            else
+            {
+                Console.WriteLine("l'iva inserita non può essere uguale o minore di 0 ");
+            }
+        }
+
+
+        //Metodi pubblici
+        public int PrezzoBase()
+        {
+            return prezzo;
+        }
+
+
+        public string NomeEsteso()
+        {
+            var concatenato = Codice + nome;
+
+            return concatenato;
+        }
+
+
+
+
+        public void StampaDatiProdotto()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("il Codice del prodotto è :" + GetCodice());
+            Console.WriteLine("il Nome :" + GetNome());
+            Console.WriteLine("Descrezione Prodotto :" + GetDescrizione());
+            Console.WriteLine("il prezzo senza iva è di :" + GetPrezzo() + " €");
+            Console.WriteLine($"Prezzo compreso iva è di:{CalcoloIva()} €");
+            Console.WriteLine("il nome esteso è :" + NomeEsteso());
+            Console.WriteLine("-----------------------");
+        }
+
+        //Metodi di Utilità
+
+        private double CalcoloIva()
+        {
+            double PIva = prezzo * iva;
+
+            double PrezzoFinale = prezzo + PIva;
+
+            PrezzoFinale = (double)System.Math.Round(PrezzoFinale, 2);
+
+            return PrezzoFinale;
+        }
+
+    }
+}
---
+++ CsharpShop2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CsharpShop2;
+
+string NomeProdotto = "Carne";
+string DiscrezioneProdotto = "cane di mucca";
+int PrezzoProdotto = 7;
+double Iva = 0.22;
+
+
+
+
+
+Prodotto InputUtente = new Prodotto(NomeProdotto, DiscrezioneProdotto, PrezzoProdotto, Iva);
+
+
+InputUtente.StampaDatiProdotto();
