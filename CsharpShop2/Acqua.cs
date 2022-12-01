382710cc500cff2dcdcc1d68aa518c8bda187b37
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-12-01 15:59:50 +0100

    aggiunti classi con sotto classe prodotto

---
+++ CsharpShop2/Acqua.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpShop2
+{
+    internal class Acqua : Prodotto
+    {
+
+        #region Proprietà
+        private double litri;
+        private double ph;
+        private string sorgente;
+        #endregion
+
+
+
+        //Costruttori
+        public Acqua(string nome, string descrizione, double prezzo, double iva, double litri, double ph, string sorgente) : base(nome, descrizione, prezzo, iva)
+        {
+            this.sorgente = sorgente;
+            this.litri = litri;
+            this.ph = ph;
+        }
+        //Getters
+
+    }
+}
--- CsharpShop2/Program.cs
+++ CsharpShop2/Program.cs
@@ -7,4 +7,4 @@ Prodotto Aqua = new Prodotto("Acqua", "Acqua Naturale", 0.60, 0.22);
 Prodotto Banana = new Prodotto("Banana", "Banana", 0.6, 0.22);
 
 
-
+Banana.StampaDatiProdotto ();
